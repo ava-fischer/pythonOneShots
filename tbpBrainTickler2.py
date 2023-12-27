ca4f1157ef1b1376474ca895d59ef2d2a71b9435
ava-fischer <agfisch@iastate.edu>
2023-12-26 22:35:51 -0600

    Flesh out solultion checks

--- tbpBrainTickler2.py
+++ tbpBrainTickler2.py
@@ -1,5 +1,9 @@
 import itertools
+from enum import Enum
 
+"""class FailType(Enum):
+    SHIPORDER = 0, 
+    CAPTAINORDER = 1"""
 class Captain:
 
     def __init__(self, name, medals, ship):
@@ -33,9 +37,12 @@ def getCaptainPosition(captainName, inputList):
             return i
     else:
         return -1
-
+    
 
 def checkRules(inputList):
+
+    testsPassing = True
+
     checkRuleI(inputList)
     checkRuleII(inputList)
     checkRuleIII(inputList)
@@ -48,30 +55,58 @@ def checkRules(inputList):
 def checkRuleI(inputList):
 
     if getShipPosition("Albacore", inputList) == getCaptainPosition("S", inputList) and getCaptainPosition("S", inputList) != 0 and getCaptainPosition("S", inputList) != 4:
-        return "PASS"
+        
     else:
-        return "FAIL"
+        testsPassing = False
 
 def checkRuleII(inputList):
     
-    if 
+    if inputList[getCaptainPosition("W", inputList)].getShip == "Hammerhead" and (getShipPosition("Hammerhead", inputList) + 1) == getCaptainPosition("T", inputList) and  inputList[getCaptainPosition("T", inputList)].getShip != "Bass":
+        return "PASS"
+    else:
+        testsPassing = False
 
 def checkRuleIII(inputList):
 
     if getShipPosition("Coho", inputList) == 4:
         return "PASS"
     else:
-        return "FAIL"
+        testsPassing = False
 
 def checkRuleIV(inputList):
 
+    if getCaptainPosition("U", inputList) == (getCaptainPosition("V", inputList) + 1):
+        return "PASS"
+    else:
+        testsPassing = False
+
 def checkRuleV(inputList):
+
+    if getCaptainPosition("X", inputList) == (getShipPosition("Eel", inputList) - 1) and getCaptainPosition("X", inputList) == (getShipPosition("Dogfish", inputList) + 1):
+        return "PASS"
+    else:
+        testsPassing = False
 
 def checkRuleVI(inputList):
 
+    if getCaptainPosition("Y", inputList) == 3:
+        return "PASS"
+    else:
+        testsPassing = False
+
 def checkRuleVII(inputList):
 
+    if inputList[1].getMedals < inputList[getShipPosition("Flounder", inputList)].getMedals and getShipPosition("Flounder", inputList) != 0 and getShipPosition("Flounder", inputList) != 2:
+        return "PASS"
+    else:
+        testsPassing = False
+
 def checkRuleVIII(inputList):
+
+    if inputList[0].getMedals(inputList[0]) > 4:
+        return "PASS"
+    else:
+        testsPassing = False
 
     
 s = Captain("S", 6, "")
@@ -85,6 +120,8 @@ z = Captain("Z", 6, "")
 
 captains = list(itertools.permutations([s, t, u, v, w, x, y, z]))
 ships = list(itertools.permutations(["Albacore", "Hammerhead", "Bass", "Coho", "Dogfish", "Eel", "Gar", "Flounder"]))
+
+solutionFound = False
 
 for permCaptains in captains:
     #result will be list of Captains
