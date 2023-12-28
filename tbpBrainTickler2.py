e4afcaff9a8e9f0746334b4dceccd2face7d4887
ava-fischer <agfisch@iastate.edu>
2023-12-27 20:31:31 -0600

    Produces a correct solution; minimally optimized

--- tbpBrainTickler2.py
+++ tbpBrainTickler2.py
@@ -10,6 +10,9 @@ class Captain:
         self.name = name
         self.medals = medals
         self.ship = ship
+    
+    def __str__(self):
+        return f"Captain: {self.name}, Ship: {self.ship}"
 
     def getMedals(self):
         return self.medals
@@ -26,85 +29,107 @@ class Captain:
 
 def getShipPosition(shipName, inputList):
     for i in range(len(inputList)):
-        if inputList[i].getShip(inputList[i]) == shipName:
+        if inputList[i].getShip() == shipName:
             return i
     else:
         return -1
     
 def getCaptainPosition(captainName, inputList):
     for i in range(len(inputList)):
-        if inputList[i].getName(inputList[i]) == captainName:
+        if inputList[i].getName() == captainName:
             return i
     else:
         return -1
     
+def printCaptains(captains):
+    for i in range(len(captains)):
+        print(captains[i])
 
 def checkRules(inputList):
 
+    global testsPassing
     testsPassing = True
 
-    checkRuleI(inputList)
-    checkRuleII(inputList)
-    checkRuleIII(inputList)
-    checkRuleIV(inputList)
-    checkRuleV(inputList)
-    checkRuleVI(inputList)
-    checkRuleVII(inputList)
-    checkRuleVIII(inputList)
+    checks = [checkRuleI, checkRuleII, checkRuleIII, checkRuleIV, checkRuleV, checkRuleVI, checkRuleVII]
+
+    for check in checks:
+        check(inputList)
+        if testsPassing == False:
+            break
+
+    if testsPassing:
+        return "SOLUTION FOUND"
+
 
 def checkRuleI(inputList):
 
-    if getShipPosition("Albacore", inputList) == getCaptainPosition("S", inputList) and getCaptainPosition("S", inputList) != 0 and getCaptainPosition("S", inputList) != 4:
-        
+    global testsPassing
+
+    if getShipPosition("Albacore", inputList) == (getCaptainPosition("S", inputList) + 2) and getCaptainPosition("S", inputList) != 0 and getCaptainPosition("S", inputList) != 4:
+        return
     else:
         testsPassing = False
 
 def checkRuleII(inputList):
+
+    global testsPassing
     
-    if inputList[getCaptainPosition("W", inputList)].getShip == "Hammerhead" and (getShipPosition("Hammerhead", inputList) + 1) == getCaptainPosition("T", inputList) and  inputList[getCaptainPosition("T", inputList)].getShip != "Bass":
-        return "PASS"
+    if inputList[getCaptainPosition("W", inputList)].getShip() == "Hammerhead" and getShipPosition("Hammerhead", inputList) == (getCaptainPosition("T", inputList) + 1) and  inputList[getCaptainPosition("T", inputList)].getShip() != "Bass":
+        return
     else:
         testsPassing = False
 
 def checkRuleIII(inputList):
 
+    global testsPassing
+
     if getShipPosition("Coho", inputList) == 4:
-        return "PASS"
+        return
     else:
         testsPassing = False
 
 def checkRuleIV(inputList):
 
+    global testsPassing
+
     if getCaptainPosition("U", inputList) == (getCaptainPosition("V", inputList) + 1):
-        return "PASS"
+        return
     else:
         testsPassing = False
 
 def checkRuleV(inputList):
 
+    global testsPassing
+
     if getCaptainPosition("X", inputList) == (getShipPosition("Eel", inputList) - 1) and getCaptainPosition("X", inputList) == (getShipPosition("Dogfish", inputList) + 1):
-        return "PASS"
+        return 
     else:
         testsPassing = False
 
 def checkRuleVI(inputList):
 
+    global testsPassing
+
     if getCaptainPosition("Y", inputList) == 3:
-        return "PASS"
+        return 
     else:
         testsPassing = False
 
 def checkRuleVII(inputList):
 
-    if inputList[1].getMedals < inputList[getShipPosition("Flounder", inputList)].getMedals and getShipPosition("Flounder", inputList) != 0 and getShipPosition("Flounder", inputList) != 2:
-        return "PASS"
+    global testsPassing
+
+    if inputList[1].getMedals() < inputList[getShipPosition("Flounder", inputList)].getMedals() and getShipPosition("Flounder", inputList) != 0 and getShipPosition("Flounder", inputList) != 2:
+        return
     else:
         testsPassing = False
 
 def checkRuleVIII(inputList):
 
-    if inputList[0].getMedals(inputList[0]) > 4:
-        return "PASS"
+    global testsPassing
+
+    if inputList[0].getMedals() > 4:
+        return
     else:
         testsPassing = False
 
@@ -122,13 +147,43 @@ captains = list(itertools.permutations([s, t, u, v, w, x, y, z]))
 ships = list(itertools.permutations(["Albacore", "Hammerhead", "Bass", "Coho", "Dogfish", "Eel", "Gar", "Flounder"]))
 
 solutionFound = False
+checkID = 0
 
 for permCaptains in captains:
-    #result will be list of Captains
-    for captain in permCaptains:
-        #result will be a Captain
-        for permShips in ships:
-            #result will be list of ships (Strings)
-            for ship in permShips:
-                captain.setShip(captain, ship)
-    checkRules(permCaptains)
+    #for every permutation of Captains... (result will be list of Captains)
+
+    #knock out some permutations on the basis of incorrect captain positions
+    if permCaptains[0].getMedals() < 5:
+        continue
+    elif permCaptains[3].getName() != "Y":
+        continue
+    elif permCaptains[0].getName() == "S" or permCaptains[4].getName() == "S":
+        continue
+    
+
+    for permShips in ships:
+
+        """#knock out some permutations on the basis of incorrect ship position
+        if getShipPosition("Coho", permShips) != 4:
+            continue
+        elif getShipPosition("Flounder", permShips) == 0 or getShipPosition("Flounder", permShips) == 2:
+            continue
+        elif getCaptainPosition("W", permCaptains) != getShipPosition("Hammerhead", permShips):
+            continue"""
+        
+        #for every permutation of Ships... (result will be list of ships (Strings))
+        for i in range(len(permShips)):
+            permCaptains[i].setShip(permShips[i])
+            
+        #print("Check ID: " + str(checkID))
+        checkResult = checkRules(permCaptains)
+        checkID += 1
+
+        if checkResult == "SOLUTION FOUND":
+            solutionFound = True
+            print("SOLUTION FOUND")
+            printCaptains(permCaptains)
+            exit()
+
+if not solutionFound:
+    print("No solution was found.")
